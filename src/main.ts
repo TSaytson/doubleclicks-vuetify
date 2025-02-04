f53b1c5d9268db3858eee8d1e2ec05e409bdc61b
Thiago Saytson <thiagosaytson@poli.ufrj.br>
2025-02-04 12:43:45 -0300

    [fix] import vuetify/styles/main.css at main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 
-import 'vuetify/styles'
+import 'vuetify/styles/main.css'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
